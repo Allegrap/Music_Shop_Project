07f1db3e57241a370c1b48a576d9aed58c999b48
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-03-31 14:15:31 +0100

    save method added to artist

--- models/artist.rb
+++ models/artist.rb
@@ -1,8 +1,16 @@
+require_relative('../db/sql_runner')
+
 class Artist
 
   def initialize(options)
     @id = options['id'].to_i
     @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO artists (name) VALUES ('#{@name}') RETURNING *"
+    results = SqlRunner.run(sql)
+    @id = results.first()['id'].to_i
   end
 
 
