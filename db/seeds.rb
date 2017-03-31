9d449269277f2c02765ed5f5eb9be1a4364ec5f5
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-03-31 14:23:23 +0100

    3 artists added to seeds.rb and saved

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,15 @@ require_relative('../models/album')
 require_relative('../models/artist')
 require('pry-byebug')
 
+artist1 = Artist.new({'name' => 'James Blake'})
+
+artist2 = Artist.new({'name' => 'Beyonce'})
+
+artist3 = Artist.new({'name' => 'Santana'})
+
+artist1.save()
+artist2.save()
+artist3.save()
 
 
 
