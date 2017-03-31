1c2fe81382f5baff501e42b16ae7eb6564f3b2a2
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-03-31 14:29:39 +0100

    3 albums added and saved with attr accessor added to id

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,33 @@ artist1.save()
 artist2.save()
 artist3.save()
 
+album1 = Album.new({
+  'title' => 'the colour in anything',
+  'artist_id' => artist1.id,
+  'quantity' => 43,
+  'sell_price' => 12,
+  'buy_price' => 5
+  })
 
+album2 = Album.new({
+  'title' => 'lemonade',
+  'artist_id' => artist2.id,
+  'quantity' => 23,
+  'sell_price' => 17,
+  'buy_price' => 8
+  })
+
+album3 = Album.new({
+  'title' => 'supernatural',
+  'artist_id' => artist3.id,
+  'quantity' => 13,
+  'sell_price' => 9,
+  'buy_price' => 4
+  })
+
+album1.save()
+album2.save()
+album3.save()
 
 
 binding.pry
--- models/artist.rb
+++ models/artist.rb
@@ -2,6 +2,8 @@ require_relative('../db/sql_runner')
 
 class Artist
 
+  attr_reader :id
+
   def initialize(options)
     @id = options['id'].to_i
     @name = options['name']
